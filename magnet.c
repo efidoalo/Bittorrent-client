4a5cb39f8ac6932963202256be998efdc3d0ae70
Andy Oldham <andyoldham1@hotmail.co.uk>
2025-06-06 20:49:23 +0100

    Add files via upload

---
+++ magnet.c
@@ -0,0 +1,124 @@
+/*===============================
+ *
+ * File: magnet.c
+ * Content: Implementation of magnet.h functions
+ * to process magnet links.
+ * Date: 6/6/2025
+ *
+ *********************************/ 
+
+#include "magnet.h"
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+uint8_t btih_present(char *magnet_link)
+{
+	char *btih_magnet = "xt=urn:btih";
+	size_t magnet_link_len = strlen(magnet_link);
+	size_t btih_prefix_len = strlen(btih_magnet);
+	int res = 0;
+	for (int i=0; i<=(magnet_link_len - btih_prefix_len); ++i) {
+		if (strncmp(&(magnet_link[i]), btih_magnet, btih_prefix_len) == 0) {
+			res = 1;
+			return res;
+		}
+	}
+	return res;
+}
+
+uint8_t *magnet_info_hash(char *magnet_link, uint8_t type)
+{
+	char *magnet_prefix = "magnet:?";
+	char *btih_prefix = "xt=urn:btih:";
+	char *btmh_prefix = "xt=urn:btmh:";
+	size_t magnet_prefix_len = strlen(magnet_prefix);
+	size_t btih_prefix_len = strlen(btih_prefix);
+	size_t btmh_prefix_len = strlen(btmh_prefix);
+	if (strnmcp(magnet_link, magnet_prefix, magnet_prefix_len) == 0) {
+		size_t magnet_link_len = strlen(magnet_link);
+		if (type == 1) {
+			// btih type specified
+			int index = 0;
+			for (index; index <= (magnet_link_len - btih_prefix_len); ++index) {
+				if (strncmp(&(magnet_link[index]), btih_prefix, btih_prefix_len)==0) {
+					int info_hash_len = 0;
+					index += btih_prefix_len;
+					int init_index = index;
+					char *display_name_prefix = "&dn=";
+					char *announce_suffix = "announce";
+					size_t display_name_prefix_len = strlen(display_name_prefix);
+					size_t announce_suffix_len = strlen(announce_suffix);
+					for (index; index<magnet_link_len; ) {
+						if (strncmp(&(magnet_link[index]), display_name_prefix, display_name_prefix_len)==0) {
+							break;
+						}
+						else if (strncmp(&(magnet_link[index]), announce_suffix, announce_suffix_len)==0) {
+							break;
+						}
+						++index;
+					}
+					info_hash_len = index - init_index;	
+					if (info_hash_len == 40) {
+						// info hash is hex encoded
+						uint8_t *info_hash_raw_digest = (uint8_t *)malloc(20);
+					        if (info_hash_raw_digest = NULL) {
+							printf("Error on calling malloc to allocate memory for sha1 digest. %s.\n". strerror(errno));
+							return 0;
+						}
+						for (int i=0; i<20; ++i) {
+							uint8_t curr_byte = 0;
+							char high_4_bits = magnet_link[init_index + (i*2)];
+							char low_4_bits = magnet_link[init_index + (i*2) + 1];
+							if (high_4_bits >= 97) {
+								curr_byte += ((uint8_t)(high_4_bits - 97 + 10) << 4);
+							}
+							else if (high_4_bits >= 65) {
+								curr_byte += ((uint8_t)(high_4_bits - 65 + 10) << 4);
+							}
+							else if (high_4_bits >= 48) {
+								curr_byte += ((uint8_t)(high_4_bits - 48 + 10) << 4);
+							}
+							else {
+								// invalid hex character (supposed to be 0-9, a-f, or A-F)
+								printf("Invalid hexadecimal character found in sha1 diges of info hash which is supposed"
+								       " to be hex encoded");
+								free(info_hash_raw_digest);
+								return NULL;
+							}
+							if (low_4_bits >= 97) {
+                                                                curr_byte += (uint8_t)(high_4_bits - 97 + 10);
+                                                        }
+                                                        else if (low_4_bits >= 65) {
+                                                                curr_byte += (uint8_t)(high_4_bits - 65 + 10);
+                                                        }
+                                                        else if (low_4_bits >= 48) {
+                                                                curr_byte += (uint8_t)(high_4_bits - 48 + 10);
+                                                        }
+                                                        else {
+                                                                // invalid hex character (supposed to be 0-9, a-f, or A-F)
+                                                                printf("Invalid hexadecimal character found in sha1 diges of info hash which is supposed"
+                                                                       " to be hex encoded");
+                                                                free(info_hash_raw_digest);
+                                                                return NULL;
+                                                        }
+							info_hash_raw_digest[i] = curr_byte;
+						}
+						return info_hash_raw_digest;	
+					}
+					// base32 encoding of sha1 hash
+				}		
+			}
+		}
+		else if (type == 0) {
+			// btmh specified
+		}
+		else {
+			return 0; // ill formed type arguement
+		}
+	}
+	else {
+		return 0; // return 0 if the magnet link does not start with
+			  // the magnet_prefix string
+	}
+}
---
+++ magnet.h
@@ -0,0 +1,59 @@
+/*===========================;
+ *
+ * File: magnet.h
+ * Content: header file for functions to
+ * parse a megnet link.
+ * Date: 6/6/2025
+ *
+ ***************************************/
+
+#include "~/Documents/Containers/C/vector.h"
+
+#ifndef __MAGNET_H_INCLUDED__
+#define __MAGNET_H_INCLUDED__
+
+struct tracker
+{
+	char *scheme;  // either "udp" or "tcp"
+	char *url;
+	uint16_t port;
+};
+
+struct peer
+{
+	char *host; // either hostname, ipv4 or ipv6 literal
+	uint16_t port;
+};
+
+// returns 1 if magnet link includes a btih formatted link.
+// otherwsie returns 0
+uint8_t btih_present(char *magnet_link);
+
+// Function that returns the 20 byte sha1 hash contained
+// within the portion of the magnet link of type type as raw binary data.
+// (type = 1 for btih, 0 for btmh). type portion of magnet link is assumed
+// to have an info hash. Function returns null pointer if magnet_link is 
+// illformedi or if type is not 1 or 0.
+uint8_t *magnet_info_hash(char *magnet_link, uint8_t type);
+
+// type is 0 for btmh, 1 for btih,
+// the function checks for a tracker list in the magnet link portion
+// of type given by the type parameter.
+// returns 1 if there exists a tracker list, 0 otherwise
+uint8_t magnet_contains_tracker_list(char *magnet_link, uint8_t type);
+
+// returns a vector of trackers within the portion of the magnet link
+// that has type type where type = 1 for btih, 0 for btmh. Assumes the given
+// portion of the magnet link includes tracker data.
+struct vector *get_tracker_vector(char *magnet_link, uint8_t type); 
+
+// function that returns 1 if the magnet link portion with type type 
+// (1 for btih, 0 for btmh) has any peers, 0 otherwise. 
+uint8_t magnet_contains_peer_list(char *magnet_link, uint8_t type);
+
+// returns a vector of peers from the portion of the magnet link with type type
+// (1 for btih, 0 for btmh). Assumes the given portion of the magnet link has
+// peer data.
+struct vector *get_peer_vector(char *magnet_link, uint8_t type);
+#endif
+
