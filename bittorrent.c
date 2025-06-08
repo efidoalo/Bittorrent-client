eb5a5025573d14025190d9ab6b4a49d54c909f6c
Andy Oldham <andyoldham1@hotmail.co.uk>
2025-06-08 17:21:10 +0100

    initial commit of partial master.c and master.h

---
+++ bittorrent.c
@@ -0,0 +1,11 @@
+/*=====================================;
+ *
+ * File: bittorrent.c
+ * Content: File for downloading and
+ * uploading files via the peer to peer
+ * network protocol Bittorrent.
+ * Date: 6/6/2025
+ *
+ **************************************/
+
+
